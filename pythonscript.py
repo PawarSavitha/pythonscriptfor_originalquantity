53631d6eb4f843c46aac5bba98fc9323d31ed6ee
PawarSavitha <savithapawar038@gmail.com>
2023-09-19 12:12:12 +0530

    addind python script

---
+++ pythonscript.py
@@ -0,0 +1,23 @@
+import xml.etree.ElementTree as ET
+
+# Load the XML file
+tree = ET.parse('a.xml')
+root = tree.getroot()
+
+# Iterate through each <paymentItem> element
+for payment_item in root.findall('.//paymentItem'):
+    # Check if <originalQuantity> element exists
+    original_quantity_element = payment_item.find('originalQuantity')
+    if original_quantity_element is None:
+        # If <originalQuantity> is missing, create it with a value of 0
+        original_quantity_element = ET.Element('originalQuantity')
+        original_quantity_element.text = '0'
+        
+        # Find the <completedQuantity> element
+        completed_quantity_element = payment_item.find('completedQuantity')
+        
+        # Insert <originalQuantity> before <completedQuantity>
+        payment_item.insert(list(payment_item).index(completed_quantity_element), original_quantity_element)
+
+# Save the updated XML file
+tree.write('PA_01072023_123023.xml')
